690b0ad8bb5f253dbb1cb8517ef09c47b4cad9e5
alexweher <alexweher@yandex.ru>
2024-10-16 21:14:09 +0300

    Begin IT tests

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -2,10 +2,8 @@ package com.example.userservice.controller;
 
 
 import com.example.userservice.model.User;
-import com.example.userservice.repository.UserRepository;
 import com.example.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
